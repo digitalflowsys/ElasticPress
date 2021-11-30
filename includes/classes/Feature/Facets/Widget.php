ca7d63b39466f1ae806c964e465b8153b45c0a7f
Dian Fishekqi <dianfishekqi@Dians-MacBook-Air.local>
2021-11-30 14:24:00 +0100

    fix

--- includes/classes/Feature/Facets/Widget.php
+++ includes/classes/Feature/Facets/Widget.php
@@ -140,176 +140,19 @@ class Widget extends WP_Widget {
 
 		echo wp_kses_post( $args['before_widget'] );
 
-		if ( ! empty( $instance['title'] ) ) {
-			echo wp_kses_post( $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'] );
-		}
-
-		$taxonomy_object = get_taxonomy( $taxonomy );
-
-		/**
-		 * Filter facet search threshold
-		 *
-		 * @hook ep_facet_search_threshold
-		 * @param  {int} $search_threshold Search threshold
-		 * @param  {string} $taxonomy Current taxonomy
-		 * @return  {int} New threshold
-		 */
-		$search_threshold = apply_filters( 'ep_facet_search_threshold', 15, $taxonomy );
-		?>
-
-		<div class="terms <?php if ( count( $terms_by_slug ) > $search_threshold ) : ?>searchable<?php endif; ?>">
-			<?php if ( count( $terms_by_slug ) > $search_threshold ) : ?>
-				<?php // translators: Taxonomy Name ?>
-				<input class="facet-search" type="search" placeholder="<?php printf( esc_html__( 'Search %s', 'elasticpress' ), esc_attr( $taxonomy_object->labels->name ) ); ?>">
-				<?php
-			endif;
-			ob_start();
-			?>
-
-			<div class="inner">
-				<?php if ( ! empty( $selected_filters['taxonomies'][ $taxonomy ] ) ) : ?>
-					<?php
-					foreach ( $selected_filters['taxonomies'][ $taxonomy ]['terms'] as $term_slug => $value ) :
-						if ( ! empty( $outputted_terms[ $term_slug ] ) ) {
-							continue;
-						}
-
-						$term = $terms_by_slug[ $term_slug ];
-
-						if ( empty( $term->parent ) && empty( $term->children ) ) {
-							$outputted_terms[ $term_slug ] = $term;
-							$new_filters                   = $selected_filters;
-
-							if ( ! empty( $new_filters['taxonomies'][ $taxonomy ] ) && ! empty( $new_filters['taxonomies'][ $taxonomy ]['terms'][ $term_slug ] ) ) {
-								unset( $new_filters['taxonomies'][ $taxonomy ]['terms'][ $term_slug ] );
-							}
-
-							// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-							echo $this->get_facet_term_html(
-								$term,
-								$feature->build_query_url( $new_filters ),
-								true
-							);
-							// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
-						} else {
-							/**
-							 * This code is so that when we encounter a selected child/parent term, we push it's whole branch
-							 * to the top. Very very painful.
-							 */
-							$top_of_tree = $term;
-							$i           = 0;
-
-							/**
-							 * Get top of tree
-							 */
-							while ( true && $i < 10 ) {
-								if ( ! empty( $term->parent_slug ) ) {
-									$top_of_tree = $terms_by_slug[ $term->parent_slug ];
-								} else {
-									break;
-								}
-
-								$i++;
-							}
-
-							$flat_ordered_terms = array();
-
-							$flat_ordered_terms[] = $top_of_tree;
-
-							$to_process = $this->order_by_selected( $top_of_tree->children, $selected_filters['taxonomies'][ $taxonomy ]['terms'], $order, $orderby );
-
-							while ( ! empty( $to_process ) ) {
-								$term = array_shift( $to_process );
-
-								$flat_ordered_terms[] = $term;
-
-								if ( ! empty( $term->children ) ) {
-									$to_process = array_merge( $this->order_by_selected( $term->children, $selected_filters['taxonomies'][ $taxonomy ]['terms'], $order, $orderby ), $to_process );
-								}
-							}
-
-							foreach ( $flat_ordered_terms as $term ) {
-								$selected                       = ! empty( $selected_filters['taxonomies'][ $taxonomy ]['terms'][ $term->slug ] );
-								$outputted_terms[ $term->slug ] = $term;
-								$new_filters                    = $selected_filters;
-
-								if ( $selected ) {
-									if ( ! empty( $new_filters['taxonomies'][ $taxonomy ] ) && ! empty( $new_filters['taxonomies'][ $taxonomy ]['terms'][ $term->slug ] ) ) {
-										unset( $new_filters['taxonomies'][ $taxonomy ]['terms'][ $term->slug ] );
-									}
-								} else {
-									if ( empty( $new_filters['taxonomies'][ $taxonomy ] ) ) {
-										$new_filters['taxonomies'][ $taxonomy ] = array(
-											'terms' => array(),
-										);
-									}
-
-									$new_filters['taxonomies'][ $taxonomy ]['terms'][ $term->slug ] = true;
-								}
-
-								// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-								echo $this->get_facet_term_html(
-									$term,
-									$feature->build_query_url( $new_filters ),
-									$selected
-								);
-								// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
-							}
-						}
-					endforeach;
-					?>
-				<?php endif; ?>
-
-				<?php
-				foreach ( $terms as $term ) :
-					if ( ! empty( $outputted_terms[ $term->slug ] ) ) {
-						continue;
-					}
-
-					$new_filters = $selected_filters;
-
-					if ( empty( $new_filters['taxonomies'][ $taxonomy ] ) ) {
-						$new_filters['taxonomies'][ $taxonomy ] = array(
-							'terms' => array(),
-						);
-					}
-
-					$new_filters['taxonomies'][ $taxonomy ]['terms'][ $term->slug ] = true;
-
-					// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-					echo $this->get_facet_term_html(
-						$term,
-						$feature->build_query_url( $new_filters )
-					);
-					// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
-				endforeach;
-				?>
-			</div>
-			<?php $facet_html = ob_get_clean(); ?>
-
-			<?php
-			// phpcs:disable
-			/**
-			 * Filter facet search widget HTML
-			 *
-			 * @hook ep_facet_search_widget
-			 * @param  {string} $facet_html Widget HTML
-			 * @param  {array} $selected_filters Selected filters
-			 * @param  {array} $terms_by_slug Terms by slug
-			 * @param  {array} $outputted_terms Outputted $terms
-			 * @param  {string} $title Widget title
-			 * @return  {string} New HTML
-			 */
-			echo apply_filters( 'ep_facet_search_widget', $facet_html, $selected_filters, $terms_by_slug, $outputted_terms, $instance['title'] );
-			// phpcs:enable
-			?>
+        $selected_terms = $selected_filters['taxonomies'][$taxonomy]['terms'] ?? [];
+        $terms = array_filter($terms, fn ($t) => $t->count > 0  || count($selected_terms) > 0); ?>
+		<div class="terms" data-terms='<?php  echo esc_attr(wp_json_encode([
+							'title' => $instance['title'],
+							'taxonomy' => $taxonomy,
+							'selected_terms' => array_map(fn ($t) => (string) $t, array_keys($selected_terms)),
+							'terms' => array_map(
+								fn ($t) => ['slug' => (string) ($t->slug), 'name' => $t->name],
+								$this->order_by_selected($terms ?? [], $selected_terms,$order,$orderby)
+							)
+						])); ?>'>
 		</div>
-		<?php
-
-		// Enqueue Script & Styles
-		wp_enqueue_script( 'elasticpress-facets' );
-		wp_enqueue_style( 'elasticpress-facets' );
-
+	<?php
 		echo wp_kses_post( $args['after_widget'] );
 	}
 
